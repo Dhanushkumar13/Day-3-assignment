858c56933fdcdc174c6a9d663c7ac62c5eca9d6c
Dhanushkumar13 <dhanushpappu99@gmail.com>
2023-04-13 23:21:20 +0530

    Adding files

---
+++ script.js
@@ -0,0 +1,56 @@
+let myResume={
+    "basics": {
+      "name": "Dhanush Kumar K",
+      "email": "dhanushpappu99@gamil.com",
+      "phone": 9550154510,
+      "degree": "B.E",
+      "location": {
+        "address": "No.1/30,Angalamman koil 2nd street, velachery",
+        "postalCode": 600042,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/dhanush-kumar-aa4a531b0/",
+          "github":"https://github.com/Dhanushkumar13"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Tata Consultancy services",
+        "position": "System Engineer",
+        "startDate": "2022-02-10",
+        "summary": "I have a vast experience on satisfying customer needs and performing than company's expections",
+      },
+    ],
+    "education": [
+      {
+        "institution": "KCG College Of Engineering",
+        "department": "Electronics and Instrumentation Engineering",
+        "studyType": "fulltime",
+        "batch start year": 2017,
+        "batch end year": 2021,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,html,css",
+        "level": "beginer"
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "gaming, reading books, coding",
+      }
+    ]
+  }
+  console.log(myResume);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+//for In
+// for (var key in json) {
+// if (json.hasOwnProperty(key)) {
+//   console.log(json[key].id);
+//   //console.log(json[key].msg);
+ 
+// }
+// }
+// //for Of
+// let text = "";
+// for (let x of json[key].id) {
+//  text += x; 
+// }
+//  console.log(text);
